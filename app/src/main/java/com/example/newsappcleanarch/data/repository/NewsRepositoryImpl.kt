a7666c0bc07e8e39f35b6003b3611250d77c0ab6
Paulo Henrick <pauloifgoianorv@gmail.com>
2021-09-22 22:09:23 -0300

    Create viewModel and ViewModelFactory

--- app/src/main/java/com/example/newsappcleanarch/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/newsappcleanarch/data/repository/NewsRepositoryImpl.kt
@@ -12,8 +12,8 @@ class NewsRepositoryImpl(
     private val newsRemoteDataSource: NewsRemoteDataSource
 ) : NewsRepository { // Este é o ponto de conexao entre o meu data e minha domain :D
 
-    override suspend fun getNewsHeadlines(): Resource<APIResponse> {
-        return responseToResource(newsRemoteDataSource.getTopHeadlines())
+    override suspend fun getNewsHeadlines(country: String, page: Int): Resource<APIResponse> {
+        return responseToResource(newsRemoteDataSource.getTopHeadlines(country, page))
     }
 
     private fun responseToResource(response: Response<APIResponse>): Resource<APIResponse> {
---
+++ app/src/main/java/com/example/newsappcleanarch/presentation/viewmodel/NewsViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.newsappcleanarch.presentation.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.newsappcleanarch.data.model.APIResponse
+import com.example.newsappcleanarch.data.util.Resource
+import com.example.newsappcleanarch.domain.usecase.GetNewsHeadlinesUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NewsViewModel(
+    private val app: Application,
+    private val getNewsHeadlinesUseCase: GetNewsHeadlinesUseCase
+) : AndroidViewModel(app) {
+    val newsHeadlines: MutableLiveData<Resource<APIResponse>> = MutableLiveData()
+
+    fun getNewsHeadlines(country: String, page: Int) = viewModelScope.launch(Dispatchers.IO) {
+        newsHeadlines.postValue(Resource.Loading())
+        try {
+            if (isNetworkAvailable(app.applicationContext)) {
+                val apiResult = getNewsHeadlinesUseCase.execute(country, page)
+                newsHeadlines.postValue(apiResult)
+            } else {
+                newsHeadlines.postValue(Resource.Error("Internet is not available"))
+            }
+        } catch (e: Exception) {
+            newsHeadlines.postValue(Resource.Error(e.message.toString()))
+        }
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+    }
+}
